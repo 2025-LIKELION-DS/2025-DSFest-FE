5593156724d5faa0be6cdcd0f3b615e8d0a60543
pookey1104 <pookey1104@naver.com>
2025-04-26 04:40:06 +0900

    ✨ Feat: 반응형 디자인 구현 (#7)

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom"
-import App from '@/App.jsx'
-import { register } from '@utils/serviceWorkerRegistration'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "@/App.jsx";
+import * as C from "@styles/CommonStyle.js";
+import { register } from "@utils/serviceWorkerRegistration";
 
-const root = createRoot(document.getElementById('root'))
+import YeounLogo from "@assets/responsive/title-yeoun.svg";
+import YeounText from "@assets/responsive/text-yeoun.svg";
+
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <C.Page>
+        <C.LeftContainer>
+          <C.YeounLeft src={YeounLogo} alt="여운" />
+        </C.LeftContainer>
+        <C.Phone>
+          <App />
+        </C.Phone>
+        <C.RightContainer>
+          <C.YeounRight src={YeounText} alt="덕성여대 근화제" />
+        </C.RightContainer>
+      </C.Page>
     </BrowserRouter>
-  </StrictMode>
-)
+  </StrictMode>,
+);
 
 register();
