50f6c7174c58b8c35bf591c781afb4c3cc10637b
jinhyo <kjhyo0220@gmail.com>
2025-04-25 16:47:35 +0900

    💄 Design: 사이드바 스타일링 (#4)
    기본 사이드바 스타일링

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -18,74 +18,81 @@ const Sidebar = ({ isSidebarOpen }) => {
     <>
       {isSidebarOpen && <S.Overlay />}
       <S.Sidebar>
-        <div>
+        <S.Close>
           <img src={Close} alt="닫기" />
-        </div>
+        </S.Close>
 
-        <S.Menu>
-          <img src={TimeTable} alt="타임테이블" />
-          <p>타임테이블</p>
-        </S.Menu>
+        <S.MenuList>
+          <S.Menu>
+            <img src={TimeTable} alt="타임테이블" />
+            <S.MenuTitle>타임테이블</S.MenuTitle>
+          </S.Menu>
 
-        <S.Menu>
-          <img src={Map} alt="지도" />
-          <p>지도</p>
-        </S.Menu>
+          <S.Menu>
+            <img src={Map} alt="지도" />
+            <S.MenuTitle>지도</S.MenuTitle>
+          </S.Menu>
 
-        <S.Menu>
-          <img src={Notice} alt="공지사항" />
-          <p>공지사항</p>
-        </S.Menu>
+          <S.Menu>
+            <img src={Notice} alt="공지사항" />
+            <S.MenuTitle>공지사항</S.MenuTitle>
+          </S.Menu>
 
-        <S.Menu>
-          <img src={Review} alt="후기" />
-          <p>후기</p>
-        </S.Menu>
+          <S.Menu>
+            <img src={Review} alt="후기" />
+            <S.MenuTitle>후기</S.MenuTitle>
+          </S.Menu>
 
-        <S.Menu>
-          <img src={Puzzle} alt="퍼즐게임" />
-          <p>퍼즐게임</p>
-        </S.Menu>
+          <S.Menu>
+            <img src={Puzzle} alt="퍼즐게임" />
+            <S.MenuTitle>퍼즐게임</S.MenuTitle>
+          </S.Menu>
+        </S.MenuList>
 
-        <div>
+        <S.Info>
           <div>
             <img src={Unyul} alt="운율" />
             <img src={Likelion} alt="멋사" />
           </div>
 
-          <div>
-            <hr />
+          <S.InfoContainer>
+            <S.Line></S.Line>
             <div>
-              <p>총학생회 {"<운율>"} </p>
-              <p>덕성여대 멋쟁이사자처럼</p>
+              <S.InfoTitle>총학생회 {"<운율>"} </S.InfoTitle>
+              <S.InfoTitle>덕성여대 멋쟁이사자처럼</S.InfoTitle>
             </div>
-          </div>
+          </S.InfoContainer>
 
-          <div>
+          <S.NameList>
             <div>
-              <p>Plan&Design</p>
-              <p>강승현 송은지</p>
-            </div>
-
-            <div>
-              <p>Front-end</p>
-              <p>김서윤 김진효 노진경</p>
-              <p>목소연 정서영 최윤영</p>
+              <S.Part>Plan&Design</S.Part>
+              <S.Name>강승현 송은지</S.Name>
             </div>
 
             <div>
-              <p>Back-end</p>
-              <p>고희주 김은서 이가은</p>
-              <p>장다연 조희원</p>
+              <S.Part>Front-end</S.Part>
+              <S.Name>김서윤 김진효 노진경</S.Name>
+              <S.Name>목소연 정서영 최윤영</S.Name>
             </div>
 
             <div>
-              <p>© 2025. likelion_ds all rights reserved.</p>
-              <img src={Instagram} alt="인스타그램" />
-              <img src={Kakaotalk} alt="카카오톡" />
+              <S.Part>Back-end</S.Part>
+              <S.Name>고희주 김은서 이가은</S.Name>
+              <S.Name>장다연 조희원</S.Name>
             </div>
-          </div>
-        </div>
+
+            <S.Footer>
+              <div>
+                <S.CopyRight>© 2025. likelion_ds</S.CopyRight>
+                <S.CopyRight>all rights reserved.</S.CopyRight>
+              </div>
+              <S.SNSContainer>
+                <img src={Instagram} alt="인스타그램" />
+                <img src={Kakaotalk} alt="카카오톡" />
+              </S.SNSContainer>
+            </S.Footer>
+          </S.NameList>
+        </S.Info>
       </S.Sidebar>
     </>
   );
--- src/components/Sidebar/SidebarStyle.js
+++ src/components/Sidebar/SidebarStyle.js
@@ -30,8 +30,140 @@ export const Sidebar = styled.div`
   background-position: bottom;
 `;
 
+export const Close = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  margin-top: 16px;
+  margin-right: 20px;
+
+  > img {
+    cursor: pointer;
+  }
+`;
+
+export const MenuList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+
+  margin-left: 28px;
+
+  margin-top: 43px;
+`;
+
 export const Menu = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
+`;
+
+export const MenuTitle = styled.p`
+  margin: 0;
+
+  cursor: pointer;
+
+  color: ${palette.grayscale.text33};
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 140%;
+  letter-spacing: -0.4px;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Info = styled.div`
+  margin-top: 200px;
+  margin-left: 19px;
+
+  width: 183px;
+  height: 328px;
+  border-radius: 11px;
+  background-color: ${palette.styles.white50};
+  backdrop-filter: blur(7.5px);
+
+  padding: 9px 15px;
+
+  box-sizing: border-box;
+`;
+
+export const InfoTitle = styled.p`
+  margin: 0;
+
+  color: ${palette.grayscale.text33};
+  font-size: 14px;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: -0.35px;
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 9px;
+  margin-top: 8px;
+`;
+
+export const Line = styled.div`
+  width: 3px;
+  height: 28px;
+  background-color: ${palette.grayscale.black50};
+  border-radius: 999px;
+`;
+
+export const NameList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  margin-top: 19px;
+`;
+
+export const Part = styled.p`
+  margin: 0;
+
+  color: ${palette.grayscale.text33};
+  font-size: 12px;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: -0.3px;
+`;
+
+export const Name = styled.p`
+  margin: 0;
+
+  color: ${palette.grayscale.text33};
+  font-size: 12px;
+  font-weight: 300;
+  line-height: 130%;
+  letter-spacing: -0.3px;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  gap: 22px;
+  margin-top: 38px;
+`;
+
+export const CopyRight = styled.p`
+  margin: 0;
+
+  color: ${palette.grayscale.text88};
+  font-size: 10px;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.25px;
+`;
+
+export const SNSContainer = styled.div`
+  display: flex;
+  gap: 5px;
+
+  > img {
+    width: 18px;
+    height: 18px;
+  }
 `;
