fa1fc78068d4f2ed8d870b04271af8a165c289f6
jinhyo <kjhyo0220@gmail.com>
2025-04-26 00:28:06 +0900

    ✨ Feat: 어드민용 사이드바 개발 (#4)
    어드민에서 사용 가능한 사이드바 개발

--- src/components/Sidebar/SidebarAdmin.jsx
+++ src/components/Sidebar/SidebarAdmin.jsx
@@ -1,7 +1,45 @@
-import * as S from "@components/Sidebar/SidebarAdminStyle";
+import * as S from "@components/Sidebar/SidebarStyle";
+import * as S2 from "@components/Sidebar/SidebarAdminStyle";
+import { useNavigate } from "react-router-dom";
 
-const SidebarAdmin = () => {
-  return <div>admin용 사이드바</div>;
+import Close from "@assets/sidebar/icon-close.svg";
+import Logout from "@assets/admin/icon-logout.svg";
+
+const SidebarAdmin = ({ isSidebarOpen, onClose }) => {
+  const navigate = useNavigate();
+
+  const handleLinkClick = (path) => {
+    navigate(path);
+  };
+  return (
+    <>
+      <S.Overlay onClick={onClose} />
+      <S2.Sidebar $isSidebarOpen={isSidebarOpen}>
+        <S.Close onClick={onClose}>
+          <img src={Close} alt="닫기" />
+        </S.Close>
+
+        <S.MenuList>
+          <S.Menu onClick={() => handleLinkClick("notice/new")}>
+            <S.MenuTitle>공지사항</S.MenuTitle>
+          </S.Menu>
+
+          <S.Menu onClick={() => handleLinkClick("booth/new")}>
+            <S.MenuTitle>부스 QR 및 비밀번호 생성</S.MenuTitle>
+          </S.Menu>
+
+          <S.Menu onClick={() => handleLinkClick("booth")}>
+            <S.MenuTitle>부스 QR 및 비밀번호 목록</S.MenuTitle>
+          </S.Menu>
+        </S.MenuList>
+
+        <S2.Logout>
+          <img src={Logout} alt="로그아웃" />
+          <S2.LogoutText>로그아웃</S2.LogoutText>
+        </S2.Logout>
+      </S2.Sidebar>
+    </>
+  );
 };
 
 export default SidebarAdmin;
---
+++ src/components/Sidebar/SidebarAdminStyle.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import palette from "@styles/theme";
+
+import { Sidebar as SidebarOrigin } from "@components/Sidebar/SidebarStyle";
+
+export const Sidebar = styled(SidebarOrigin)`
+  background-image: none;
+`;
+
+export const Logout = styled.div`
+  display: flex;
+  gap: 3px;
+  margin-left: 28px;
+  margin-top: 90px;
+`;
+
+export const LogoutText = styled.div`
+  color: ${palette.system.alert};
+`;
