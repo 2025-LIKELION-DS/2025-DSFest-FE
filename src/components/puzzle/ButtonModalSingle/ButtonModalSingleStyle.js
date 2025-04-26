8c9f93de8cd730cce7746d7d787af7e7a61d1398
jinhyo <kjhyo0220@gmail.com>
2025-04-27 05:31:47 +0900

    ✨ Feat: Button_modal_single 구현 (#9)
    puzzle 모달에서 사용되는 single 버튼 구현

---
+++ src/components/puzzle/ButtonModalSingle/ButtonModalSingleStyle.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import palette from '@styles/theme';
+
+export const ButtonContainer = styled.div`
+  padding: 0px 16px;
+`
+export const ButtonCommon = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 48px;  
+  border-radius: 29px;
+  outline: none;
+
+  color: ${palette.grayscale.white};
+  background-color: ${palette.mainPurple};
+
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 140%; 
+`
