538c2385e770639ddc933da4deec259ff0ef757a
jinhyo <kjhyo0220@gmail.com>
2025-05-01 02:13:26 +0900

    ✨ Feat: 메인페이지 배경 (#14)
    메인페이지 배경 작업

--- src/routes/main/Main.jsx
+++ src/routes/main/Main.jsx
@@ -1,10 +1,18 @@
 import * as M from '@main/MainStyle';
 
+import Moon from '@assets/main/moon-crescent.png';
+import Logo from '@assets/main/title-yeoun.png';
+import Cloud from '@assets/main/cloud-textured-purple_80_.png';
+import Tree from '@assets/main/tree-shadow-grain_80_.png';
+
 function Main() {
   return (
-    <>
-      <div>메인</div>
-    </>
+    <M.Main>
+      <M.MoonImg src={Moon} alt="달" />
+      <M.LogoImg src={Logo} alt="로고" />
+      <M.TreeImg src={Tree} alt="나무" />
+      <M.CloudImg src={Cloud} alt="구름" />
+    </M.Main>
   );
 }
 
--- src/routes/main/MainStyle.js
+++ src/routes/main/MainStyle.js
@@ -1,4 +1,37 @@
 import styled from 'styled-components';
 import palette from '@styles/theme';
 
-export const Main = styled.div``;
+import BackGround from '@assets/main/puzzle-scene-stars-grain.jpg';
+
+export const Main = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url(${BackGround});
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  position: relative;
+`;
+
+export const MoonImg = styled.img`
+  position: absolute;
+  top: 54px;
+  right: 27px;
+`;
+
+export const LogoImg = styled.img`
+  display: block;
+  margin: 0 auto;
+  padding-top: 23.7%;
+`;
+
+export const CloudImg = styled.img`
+  position: absolute;
+  bottom: 0;
+`;
+
+export const TreeImg = styled.img`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+`;
