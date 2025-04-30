799769ce21ef49da71927af15b971bde11ab48c6
mogg22 <ahrthdus1@gmail.com>
2025-05-01 05:05:37 +0900

    ✨ Feat: 어드민 로그인 퍼블리싱 (#3)
    어드민 로그인 완성

--- src/routes/admin/LoginStyle.js
+++ src/routes/admin/LoginStyle.js
@@ -1,4 +1,34 @@
 import styled from 'styled-components';
-import palette from '@styles/theme';
+// import palette from '@styles/theme';
 
-export const Login = styled.div``;
+export const Login = styled.div`
+  width: 100%;
+  height: calc(100% - 200px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-bottom: 200px;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 100px;
+  color: #191919;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: -0.5px;
+`;
+
+export const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  padding-bottom: 28px;
+`;
