c59550b324980ca0eb24d2d83073503bfd938eb7
mogg22 <ahrthdus1@gmail.com>
2025-05-01 04:18:51 +0900

    ✨ Feat: 공지사항 목록 구현 (#16)
    공지사항 목록 조건별 렌더링 세팅 및 퍼블리싱 완료, api 연동 구현 미완료

--- src/routes/notice/List.jsx
+++ src/routes/notice/List.jsx
@@ -1,9 +1,55 @@
+// import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+// import axios from "axios";
+import { isAdminLoggedIn } from '@utils/admin';
+import Contact from '@notice/components/Contact';
+import AdminBtn from '@components/admin/ButtonAdminSingle/ButtonAdminSingle';
+// import Content from '@notice/components/Content';
 import * as L from '@notice/ListStyle';
+import palette from '@styles/theme';
 
 function List() {
+  const navigate = useNavigate();
+  const isAdmin = isAdminLoggedIn();
+  // const [contents, setContents] = useState([]);
+
+  const handleAdminBtn = () => {
+    if (isAdminLoggedIn()) {
+      navigate('/notice/new');
+    }
+  };
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`${import.meta.env.VITE_API_URL}/api/notices`)
+  //     .then((response) => {
+  //       setContents(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("공지사항 목록을 불러오는 중 오류 발생:", error);
+  //     });
+  // }, []);
+
   return (
     <>
-      <L.List>공지사항 목록</L.List>
+      {isAdmin && (
+        <L.AdminBtnArea>
+          <AdminBtn text="작성하기" color={palette.mainPurple} onClick={handleAdminBtn} />
+        </L.AdminBtnArea>
+      )}
+      <L.List>
+        {!isAdmin && <Contact />}
+        <L.Contents>
+          {/* {contents.map((content) => (
+          <Content
+            title={content.id}
+            preview={content.}
+            previewImg={content.}
+            onClick={() => navigate(`/notice/${content.id}`)}
+          />
+        ))} */}
+        </L.Contents>
+      </L.List>
     </>
   );
 }
