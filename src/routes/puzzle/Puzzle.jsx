6b7f546e4670598b7fb722a83e8a2ba32ae1e785
mzxxzysy <jsyjj21@gmail.com>
2025-05-03 23:34:18 +0900

    ✨ Feat: 퍼즐 게임 개발 (#17)
    퍼즐 UI

--- src/routes/puzzle/Puzzle.jsx
+++ src/routes/puzzle/Puzzle.jsx
@@ -9,6 +9,36 @@ import lightIcon from '@assets/puzzle/icon-tip-lightbulb.svg';
 import check from '@assets/puzzle/check.png';
 import whiteCheck from '@assets/puzzle/white_check.png';
 
+import puzzle1Default from '@assets/puzzle/puzzle_default_1.svg';
+import puzzle2Default from '@assets/puzzle/puzzle_default_2.svg';
+import puzzle3Default from '@assets/puzzle/puzzle_default_3.svg';
+import puzzle4Default from '@assets/puzzle/puzzle_default_4.svg';
+import puzzle5Default from '@assets/puzzle/puzzle_default_5.svg';
+import puzzle6Default from '@assets/puzzle/puzzle_default_6.svg';
+import puzzle7Default from '@assets/puzzle/puzzle_default_7.svg';
+import puzzle8Default from '@assets/puzzle/puzzle_default_8.svg';
+import puzzle9Default from '@assets/puzzle/puzzle_default_9.svg';
+
+import puzzle1Click from '@assets/puzzle/puzzle_click_1.svg';
+import puzzle2Click from '@assets/puzzle/puzzle_click_2.svg';
+import puzzle3Click from '@assets/puzzle/puzzle_click_3.svg';
+import puzzle4Click from '@assets/puzzle/puzzle_click_4.svg';
+import puzzle5Click from '@assets/puzzle/puzzle_click_5.svg';
+import puzzle6Click from '@assets/puzzle/puzzle_click_6.svg';
+import puzzle7Click from '@assets/puzzle/puzzle_click_7.svg';
+import puzzle8Click from '@assets/puzzle/puzzle_click_8.svg';
+import puzzle9Click from '@assets/puzzle/puzzle_click_9.svg';
+
+import puzzle1Complete from '@assets/puzzle/puzzle_complete_1.svg';
+import puzzle2Complete from '@assets/puzzle/puzzle_complete_2.svg';
+import puzzle3Complete from '@assets/puzzle/puzzle_complete_3.svg';
+import puzzle4Complete from '@assets/puzzle/puzzle_complete_4.svg';
+import puzzle5Complete from '@assets/puzzle/puzzle_complete_5.svg';
+import puzzle6Complete from '@assets/puzzle/puzzle_complete_6.svg';
+import puzzle7Complete from '@assets/puzzle/puzzle_complete_7.svg';
+import puzzle8Complete from '@assets/puzzle/puzzle_complete_8.svg';
+import puzzle9Complete from '@assets/puzzle/puzzle_complete_9.svg';
+
 import ButtonCommon from '@components/ButtonCommon/ButtonCommon.jsx';
 import InputLogin from '@components/puzzle/InputLogin/InputLogin';
 import ButtonModalDual from '@components/puzzle/ButtonModalDual/ButtonModalDual';
@@ -30,145 +60,201 @@ function Puzzle() {
 
   const isLoginEnabled = inputLoginID && inputLoginPWD;
 
+  const [puzzleValue, setPuzzleValue] = useState({
+    index1: true,
+    index2: true,
+    index3: false,
+    index4: false,
+    index5: true,
+    index6: true,
+    index7: false,
+    index8: true,
+    index9: true,
+  });
+
   const [authorized, setAuthorized] = useState(true);
   //퍼즐 9개를 다 채웠을 때
   const [success, setSuccess] = useState(false);
   //퍼즐 완성을 눌렀을 때
-  const [completed, setCompleted] = useState(true);
+  const [completed, setCompleted] = useState(false);
   //경품 수령 완료했을 때
   const [end, setEnd] = useState(false);
 
   return (
-    <>
-      <P.top>
-        {completed ? (
-          <>
-            <P.semibold20>축하드립니다!</P.semibold20>
-            <P.semibold20>모든 퍼즐이 완성되었어요.</P.semibold20>
-          </>
-        ) : (
-          <>
-            <P.puzzleInfo1>아래 퍼즐을 완성하고</P.puzzleInfo1>
-            <P.puzzleInfo2>
-              <P.goMap>
-                {/* onClick={goMap} */}
-                <P.boothIcon>
-                  <img src={boothIcon} alt="총학생회 부스" />
-                </P.boothIcon>
-                <P.purplesemibold16>총학생회 부스</P.purplesemibold16>
-              </P.goMap>
-              를 방문해 경품을 수령하세요!
-            </P.puzzleInfo2>
-          </>
-        )}
-      </P.top>
-
-      {authorized ? (
-        <P.completedPuzzle>
-          <P.completedTitle>
-            <P.userInfo>
-              <P.userName>은지송</P.userName>
-              <P.regular16>님의 퍼즐</P.regular16>
-            </P.userInfo>
-            <P.todoPuzzle>
-              {end ? (
-                <>
-                  <img src={check} alt="경품 수령 완료" />
-                  <P.purpleRegular14>경품 수령 완료</P.purpleRegular14>
-                </>
-              ) : (
-                <>
-                  <P.regular14>남은 퍼즐</P.regular14>
-                  <P.todoPuzzleCount>2개</P.todoPuzzleCount>
-                </>
-              )}
-            </P.todoPuzzle>
-          </P.completedTitle>
-
-          <P.completedPuzzleBox>
-            <P.regular16>
-              현재 <P.semibold16>완성한</P.semibold16> 퍼즐
-            </P.regular16>
-            <P.puzzleCount>
-              <P.glowPuzzleIcon src={glowPuzzleIcon} />
-              <P.completedPuzzleCount>7</P.completedPuzzleCount>
-              <P.completedPuzzleCountInfo>개</P.completedPuzzleCountInfo>
-            </P.puzzleCount>
-          </P.completedPuzzleBox>
-        </P.completedPuzzle>
-      ) : (
-        <P.login>
-          <P.inputNickname>
-            <InputLogin
-              placeholder={'닉네임을 입력해주세요'}
-              type={'text'}
-              value={inputLoginID}
-              onChange={inputLoginIDChange}
-            />
-          </P.inputNickname>
-
-          <P.inputPassword>
-            <InputLogin
-              placeholder={'비밀번호를 입력해주세요'}
-              type={'password'}
-              value={inputLoginPWD}
-              onChange={inputLoginPWDChange}
-            />
-          </P.inputPassword>
-          <ButtonCommon text={'로그인'} color={isLoginEnabled ? `${palette.mainPurple}` : `${palette.grayscale.ca}`} />
-          {/*  onClick={handleLogin} */}
-        </P.login>
-      )}
-
-      <P.puzzleGame>
-        {authorized ? (
-          completed ? (
+    <P.puzzlePage>
+      <P.currentPuzzleInfo>
+        <P.top>
+          {completed ? (
             <>
-              <P.presentInfo>
+              <P.semibold20>축하드립니다!</P.semibold20>
+              <P.semibold20>모든 퍼즐이 완성되었어요.</P.semibold20>
+            </>
+          ) : (
+            <>
+              <P.puzzleInfo1>아래 퍼즐을 완성하고</P.puzzleInfo1>
+              <P.puzzleInfo2>
                 <P.goMap>
                   {/* onClick={goMap} */}
                   <P.boothIcon>
-                    <img src={boothIcon} alt="총학생회 부스" />
+                    <img src={boothIcon} alt="총학생회 본부" />
                   </P.boothIcon>
-                  <P.purplesemibold16>총학생회 부스</P.purplesemibold16>
+                  <P.purplesemibold16>총학생회 본부</P.purplesemibold16>
                 </P.goMap>
                 를 방문해 경품을 수령하세요!
-              </P.presentInfo>
+              </P.puzzleInfo2>
+            </>
+          )}
+        </P.top>
 
-              <P.CompletedInfo>
-                <img src={lightIcon} />
-                <P.regular14>경품 수령을 위서는,</P.regular14>
-                <P.regular14>
-                  <P.bold14>재학생 + 학생회비 납부 </P.bold14>인증이 필요해요.
-                </P.regular14>
-                <P.bold14>'덕성포털&gt;통합정보시스템&gt;학적변경&gt;</P.bold14>
-                <P.regular14>
-                  <P.bold14>개인정보변경/지도교수 확인’ </P.bold14>화면을 미리 준비해
-                </P.regular14>
-                <P.regular14>경품을 빠르게 수령하고, 남은 축제를 즐겨보아요.</P.regular14>
-              </P.CompletedInfo>
-            </>
-          ) : end ? (
-            <>
-              <P.successPuzzle>
-                <P.whiteCheck src={whiteCheck} />
-                <P.whiteRegular16>
-                  이미 <P.whiteSemibold16>완성</P.whiteSemibold16>된 퍼즐입니다.
-                </P.whiteRegular16>
-              </P.successPuzzle>
-            </>
-          ) : (
-            <></>
-          )
+        {authorized ? (
+          <P.completedPuzzle>
+            <P.completedTitle>
+              <P.userInfo>
+                <P.userName>은지송</P.userName>
+                <P.regular16>님의 퍼즐</P.regular16>
+              </P.userInfo>
+              <P.todoPuzzle>
+                {end ? (
+                  <>
+                    <img src={check} alt="경품 수령 완료" />
+                    <P.purpleRegular14>경품 수령 완료</P.purpleRegular14>
+                  </>
+                ) : (
+                  <>
+                    <P.regular14>남은 퍼즐</P.regular14>
+                    <P.todoPuzzleCount>2개</P.todoPuzzleCount>
+                  </>
+                )}
+              </P.todoPuzzle>
+            </P.completedTitle>
+
+            <P.completedPuzzleBox>
+              <P.regular16>
+                현재 <P.semibold16>완성한</P.semibold16> 퍼즐
+              </P.regular16>
+              <P.puzzleCount>
+                <P.glowPuzzleIcon src={glowPuzzleIcon} />
+                <P.completedPuzzleCount>7</P.completedPuzzleCount>
+                <P.completedPuzzleCountInfo>개</P.completedPuzzleCountInfo>
+              </P.puzzleCount>
+            </P.completedPuzzleBox>
+          </P.completedPuzzle>
         ) : (
-          <>
-            <P.emptyPuzzle>
-              <P.whiteErrorIcon src={whiteErrorIcon} />
-              <P.whiteRegular16>로그인 후 이용가능합니다.</P.whiteRegular16>
-            </P.emptyPuzzle>
-          </>
+          <P.login>
+            <P.inputNickname>
+              <InputLogin
+                placeholder={'닉네임을 입력해주세요'}
+                type={'text'}
+                value={inputLoginID}
+                onChange={inputLoginIDChange}
+              />
+            </P.inputNickname>
+
+            <P.inputPassword>
+              <InputLogin
+                placeholder={'비밀번호를 입력해주세요'}
+                type={'password'}
+                value={inputLoginPWD}
+                onChange={inputLoginPWDChange}
+              />
+            </P.inputPassword>
+            <ButtonCommon
+              text={'로그인'}
+              color={isLoginEnabled ? `${palette.mainPurple}` : `${palette.grayscale.ca}`}
+            />
+            {/*  onClick={handleLogin} */}
+          </P.login>
         )}
-      </P.puzzleGame>
+
+        <P.puzzleGame>
+          {authorized ? (
+            completed ? (
+              <>
+                <P.presentInfo>
+                  <P.goMap>
+                    {/* onClick={goMap} */}
+                    <P.boothIcon>
+                      <img src={boothIcon} alt="총학생회 본부" />
+                    </P.boothIcon>
+                    <P.purplesemibold16>총학생회 본부</P.purplesemibold16>
+                  </P.goMap>
+                  를 방문해 경품을 수령하세요!
+                </P.presentInfo>
+
+                <P.CompletedInfo>
+                  <img src={lightIcon} />
+                  <P.regular14>경품 수령을 위서는,</P.regular14>
+                  <P.regular14>
+                    <P.bold14>재학생 + 학생회비 납부 </P.bold14>인증이 필요해요.
+                  </P.regular14>
+                  <P.bold14>'덕성포털&gt;통합정보시스템&gt;학적변경&gt;</P.bold14>
+                  <P.regular14>
+                    <P.bold14>개인정보변경/지도교수 확인’ </P.bold14>화면을 미리 준비해
+                  </P.regular14>
+                  <P.regular14>경품을 빠르게 수령하고, 남은 축제를 즐겨보아요.</P.regular14>
+                </P.CompletedInfo>
+              </>
+            ) : end ? (
+              <>
+                <P.successPuzzle>
+                  <P.whiteCheck src={whiteCheck} />
+                  <P.whiteRegular16>
+                    이미 <P.whiteSemibold16> 완성</P.whiteSemibold16>된 퍼즐입니다.
+                  </P.whiteRegular16>
+                </P.successPuzzle>
+              </>
+            ) : (
+              <P.puzzle>
+                <P.puzzleGrid>
+                  <P.puzzle1
+                    src={puzzleValue.index1 ? puzzle1Complete : puzzle1Default}
+                    style={puzzleValue.index1 ? { top: '0.3rem' } : {}}
+                  />
+                  <P.puzzle2
+                    src={puzzleValue.index2 ? puzzle2Complete : puzzle2Default}
+                    style={puzzleValue.index2 ? { top: '0.3rem' } : {}}
+                  />
+                  <P.puzzle3
+                    src={puzzleValue.index3 ? puzzle3Complete : puzzle3Default}
+                    style={puzzleValue.index3 ? { top: '0.3rem' } : {}}
+                  />
+                  <P.puzzle4
+                    src={puzzleValue.index4 ? puzzle4Complete : puzzle4Default}
+                    style={puzzleValue.index4 ? { top: '5.99rem' } : {}}
+                  />
+                  <P.puzzle5
+                    src={puzzleValue.index5 ? puzzle5Complete : puzzle5Default}
+                    style={puzzleValue.index5 ? { top: '7.3549rem' } : {}}
+                  />
+                  <P.puzzle6
+                    src={puzzleValue.index6 ? puzzle6Complete : puzzle6Default}
+                    style={puzzleValue.index6 ? { top: '5.98rem' } : {}}
+                  />
+                  <P.puzzle7
+                    src={puzzleValue.index7 ? puzzle7Complete : puzzle7Default}
+                    style={puzzleValue.index7 ? { top: '14.51rem' } : {}}
+                  />
+                  <P.puzzle8
+                    src={puzzleValue.index8 ? puzzle8Complete : puzzle8Default}
+                    style={puzzleValue.index8 ? { top: '13.18rem' } : {}}
+                  />
+                  <P.puzzle9
+                    src={puzzleValue.index9 ? puzzle9Complete : puzzle9Default}
+                    style={puzzleValue.index9 ? { top: '14.5rem' } : {}}
+                  />
+                </P.puzzleGrid>
+              </P.puzzle>
+            )
+          ) : (
+            <>
+              <P.emptyPuzzle>
+                <P.whiteErrorIcon src={whiteErrorIcon} />
+                <P.whiteRegular16>로그인 후 이용가능합니다.</P.whiteRegular16>
+              </P.emptyPuzzle>
+            </>
+          )}
+        </P.puzzleGame>
+      </P.currentPuzzleInfo>
 
       <P.endButton>
         {completed ? (
@@ -178,10 +264,10 @@ function Puzzle() {
           <ButtonCommon text={'퍼즐 완성'} color={`${palette.mainPurple}`} />
         ) : (
           //onClick={handleSuccess}
-          <ButtonCommon text={'퍼즐 완성'} color={`${palette.grayscale.ca}`} />
+          <ButtonCommon text={'퍼즐 완성'} color={end ? `${palette.grayscale.text88}` : `${palette.grayscale.ca}`} />
         )}
       </P.endButton>
-    </>
+    </P.puzzlePage>
   );
 }
 
--- src/routes/puzzle/PuzzleStyle.js
+++ src/routes/puzzle/PuzzleStyle.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 import palette from '@styles/theme';
 
-import emptyPuzzleImg from '@assets//puzzle/empty_puzzle.png';
+import emptyPuzzleImg from '@assets/puzzle/empty_puzzle.png';
+import successPuzzleImg from '@assets/puzzle/completed_puzzle.png';
+
+export const puzzlePage = styled.div`
+  height: calc(100svh - 87px);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const currentPuzzleInfo = styled.div``;
 
 export const top = styled.div`
   margin: 34px 0 24px 28px;
@@ -178,11 +188,100 @@ export const CompletedInfo = styled.div`
   margin-top: 23px;
 `;
 
-export const whiteErrorIcon = styled.img``;
+export const whiteErrorIcon = styled.img`
+  margin-bottom: 6px;
+`;
 
-export const successPuzzle = styled.div``;
+export const successPuzzle = styled.div`
+  width: 90vw;
+  aspect-ratio: 1;
+  background-image: url(${successPuzzleImg});
 
-export const whiteCheck = styled.img``;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const whiteCheck = styled.img`
+  margin-bottom: 6px;
+`;
+
+export const puzzle = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const puzzleGrid = styled.div`
+  width: 100vw;
+  aspect-ratio: 1;
+  position: relative;
+  right: 5px;
+  max-height: 345px;
+  max-width: 345px;
+
+  @media (max-width: 380px) {
+    transform: scale(0.85);
+  }
+`;
+
+export const puzzle1 = styled.img`
+  position: absolute;
+`;
+
+export const puzzle2 = styled.img`
+  position: absolute;
+  top: 0;
+  left: 7.0155em;
+`;
+
+export const puzzle3 = styled.img`
+  position: absolute;
+  top: 0;
+  left: 12.89em;
+`;
+
+export const puzzle4 = styled.img`
+  position: absolute;
+  top: 5.69em;
+  left: 0;
+`;
+
+export const puzzle5 = styled.img`
+  position: absolute;
+  top: 7.0549em;
+  left: 5.7em;
+`;
+
+export const puzzle6 = styled.img`
+  position: absolute;
+  top: 5.68em;
+  left: 14.2em;
+`;
+
+export const puzzle7 = styled.img`
+  position: absolute;
+  top: 14.21em;
+  left: 0;
+`;
+
+export const puzzle8 = styled.img`
+  position: absolute;
+  top: 12.88em;
+  left: 7.038em;
+`;
+
+export const puzzle9 = styled.img`
+  position: absolute;
+  top: 14.2em;
+  left: 12.87em;
+`;
 
 export const semibold20 = styled.div`
   color: ${palette.grayscale.black};
@@ -200,12 +299,15 @@ export const whiteRegular16 = styled.div`
   font-weight: 400;
   color: ${palette.grayscale.white};
   font-size: 16px;
+  display: flex;
+  flex-direction: row;
 `;
 
 export const whiteSemibold16 = styled.div`
   font-weight: 600;
   color: ${palette.grayscale.white};
   font-size: 16px;
+  margin-left: 2px;
 `;
 
 export const bold14 = styled.div`
@@ -232,6 +334,7 @@ export const purpleRegular14 = styled.div`
   color: ${palette.mainPurple};
   font-weight: 400;
   font-size: 14px;
+  margin-left: 2px;
 `;
 
 export const purplesemibold16 = styled.div`
