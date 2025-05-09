c3828e9f56a1b0f96de2d5d6107cbe6d8adab574
mogg22 <ahrthdus1@gmail.com>
2025-05-10 02:00:04 +0900

    🚧 Chore: pwa 캐시 개발 단계 간소화
    동적 파일명에 의하여 개발 단계에서 캐시 간소화하도록 수정

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,39 +1,55 @@
 const CACHE_NAME = 'my-cache-v1';
-const FILES_TO_CACHE = ['/', '/index.html', '/main.js', '/style.css'];
+const FILES_TO_CACHE = ['/', '/index.html'];
 
-// 설치: 필요한 파일 캐시
+// // 설치: 필요한 파일 캐시
+// self.addEventListener('install', (event) => {
+//   console.log('[SW] Installing and caching...');
+//   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
+//   self.skipWaiting(); // 설치되자마자 활성화
+// });
+
+// // 활성화: 오래된 캐시 정리
+// self.addEventListener('activate', (event) => {
+//   console.log('[SW] Activating...');
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) =>
+//       Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (cacheName !== CACHE_NAME) {
+//             console.log('[SW] Deleting old cache:', cacheName);
+//             return caches.delete(cacheName);
+//           }
+//         }),
+//       ),
+//     ),
+//   );
+// });
+
+// // 요청: 캐시 먼저, 없으면 네트워크
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((cachedResponse) => {
+//       if (cachedResponse) {
+//         console.log('[SW] Serving from cache:', event.request.url);
+//         return cachedResponse;
+//       }
+//       return fetch(event.request);
+//     }),
+//   );
+// });
+
+// 개발 단계에서 빌드 파일명이 계속 변경되므로, 캐시 최소화
 self.addEventListener('install', (event) => {
-  console.log('[SW] Installing and caching...');
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting(); // 설치되자마자 활성화
-});
-
-// 활성화: 오래된 캐시 정리
-self.addEventListener('activate', (event) => {
-  console.log('[SW] Activating...');
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('[SW] Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        }),
-      ),
-    ),
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    }),
   );
+  self.skipWaiting();
 });
 
-// 요청: 캐시 먼저, 없으면 네트워크
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        console.log('[SW] Serving from cache:', event.request.url);
-        return cachedResponse;
-      }
-      return fetch(event.request);
-    }),
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)))),
   );
 });
