bf412cf7f22244b6c2e911440415e9fae0c4c9fe
chldsbdud <cyy2532@gmail.com>
2025-05-16 18:25:14 +0900

    ✨ Feat: Timeline ease-in 추가

--- src/components/TimeTable/TLSchedule.jsx
+++ src/components/TimeTable/TLSchedule.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import palette from '@styles/theme';
+import { useEffect, useRef, useState } from 'react';
 
 const ScheduleBox = styled.div`
   position: absolute;
@@ -22,11 +23,60 @@ const ScheduleBox = styled.div`
   white-space: ${(props) => (props.width <= 90 ? 'pre-line' : 'nowrap')};
   box-sizing: border-box; //박스 사이즈 고정
   z-index: 2;
-`;
 
+  /* ease-in 추가 */
+  transform: translateY(10px);
+  transition:
+    top 0.3s ease-in,
+    left 0.3s ease-in,
+    width 0.3s ease-in,
+    //opacity 1s ease-in, 적용되는 게 하나 밖에 없어서 일단 주석
+    transform 1s ease-in;
+
+  &.visible {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 const TimeLineScheduleBox = ({ top, left, width, label, color }) => {
+  const boxRef = useRef(null);
+  const [opacity, setOpacity] = useState(0);
+  const [translateY, setTranslateY] = useState(10);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!boxRef.current) return;
+      const rect = boxRef.current.getBoundingClientRect();
+      const centerY = window.innerHeight / 2;
+      const boxCenterY = rect.top + rect.height / 2;
+      const distance = Math.abs(centerY - boxCenterY);
+      const maxDistance = centerY;
+
+      const ratio = Math.max(0, 1 - distance / maxDistance); // 1에 가까울수록 중앙에 있음
+      const safeOpacity = Math.max(ratio, 1); // 최소 0.3 이상
+      const translateY = (1 - ratio) * 10;
+
+      setOpacity(safeOpacity);
+      setTranslateY(translateY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <ScheduleBox top={top} left={left} width={width} color={color}>
+    <ScheduleBox
+      ref={boxRef}
+      style={{
+        opacity,
+        transform: `translateY(${translateY}px)`,
+      }}
+      top={top}
+      left={left}
+      width={width}
+      color={color}>
       {label}
     </ScheduleBox>
   );
